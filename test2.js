4cce8788071140cb636f682c0adce202e4a1dae2
oscar-mugambi <oscarmugambi@gmail.com>
2022-02-25 23:15:16 +0300

    it works

--- test2.js
+++ test2.js
@@ -1,11 +1,30 @@
 const fruitsToGet = ['apple', 'grape', 'pear'];
+const fruitBasket = {
+  apple: 23,
+  grape: 2,
+  pear: 10,
+};
+
+const sleep = (ms) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+};
+
+const getFruitNum = (fruit) => {
+  return sleep(2000).then(() => fruitBasket[fruit]);
+};
 
 const forLoop = async () => {
   console.log('start');
 
-  for (const key in fruitsToGet) {
-    console.log(key);
+  for (const el of fruitsToGet) {
+    await getFruitNum(el).then((num) => {
+      console.log(num);
+    });
   }
+
+  console.log('end');
 };
 
 forLoop();
