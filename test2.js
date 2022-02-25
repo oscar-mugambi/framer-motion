de82ffa11dd120f1274a3635c4aac0456a41ed67
oscar-mugambi <oscarmugambi@gmail.com>
2022-02-25 23:08:16 +0300

    testing async await inside loops

--- test2.js
+++ test2.js
@@ -1,1 +1,11 @@
 const fruitsToGet = ['apple', 'grape', 'pear'];
+
+const forLoop = async () => {
+  console.log('start');
+
+  for (const key in fruitsToGet) {
+    console.log(key);
+  }
+};
+
+forLoop();
