7433826e2e1e4366d51d24de9418f8b15cc9c250
oscar-mugambi <oscarmugambi@gmail.com>
2022-03-04 22:01:03 +0300

    added rotation

--- test.js
+++ test.js
@@ -4,6 +4,8 @@ const fruitInfo = {
   peach: 23,
 };
 
+console.log('start');
+
 const sleep = (ms) => {
   return new Promise((resolve) => {
     setTimeout(resolve, ms);
@@ -14,11 +16,12 @@ const getNumFruit = (fruit) => {
   return fruitInfo[fruit];
 };
 
-sleep(2000).then(() => {
-  console.log();
+sleep(2000).getNumFruit(() => {
   console.log('here we are');
 });
 
 console.log(getNumFruit('apple'));
 console.log(getNumFruit('orange'));
 console.log(getNumFruit('peach'));
+
+console.log('end');
